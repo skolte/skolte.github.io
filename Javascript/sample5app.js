053ba6f2f31374db7a3d9aefe03aefaf255c6825
skolte <sandeep.kolte@gmail.com>
2014-06-12 23:17:59 -0700

    Updated Sample5
    Added AngularJS expressions.

--- Javascript/sample5app.js
+++ Javascript/sample5app.js
@@ -1,1 +1,45 @@
-var app = angular.module('gemStore', []);
+// Code goes here
+function Cntl2($scope) {
+  var exprs = $scope.exprs = [];
+  $scope.expr = '3*10|currency';
+  $scope.addExp = function(expr) {
+    exprs.push(expr);
+  };
+
+  $scope.removeExp = function(index) {
+    exprs.splice(index, 1);
+  };
+}
+
+var EventController = function($scope) {
+    /*
+     * expose the event object to the scope
+     */
+    $scope.clickMe = function(clickEvent) {
+      $scope.clickEvent = simpleKeys(clickEvent);
+      console.log(clickEvent);
+    };
+
+    /*
+     * return a copy of an object with only non-object keys
+     * we need this to avoid circular references
+     */
+    function simpleKeys (original) {
+      return Object.keys(original).reduce(function (obj, key) {
+        obj[key] = typeof original[key] === 'object' ? '{ ... }' : original[key];
+        return obj;
+      }, {});
+    }
+  };
+  
+var OneTimeBindingController = function($scope) {
+    var counter = 0;
+    var names = ['Sachin', 'Brian', 'Sunil', 'Rahul'];
+    /*
+     * expose the event object to the scope
+     */
+    $scope.clickMe = function(clickEvent) {
+      $scope.name = names[counter % names.length];
+      counter++;
+    };
+  };
